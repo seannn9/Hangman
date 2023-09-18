12f2f2a20bbee643214c9d540e533a202e5d7616
seannn9 <naeskirlu9@gmail.com>
2023-09-18 11:28:28 +0800

    added hangman visuals

--- main.py
+++ main.py
@@ -6,18 +6,36 @@ def generate():
     word_guess = response.json()[0]
     return word_guess
 
+def draw(error):
+    if error == 0:
+        print("----------\n|\n|\n|\n|\n|\n|")
+    elif error == 1:
+        print("----------\n|        |\n|\n|\n|\n|\n|")
+    elif error == 2:
+        print("----------\n|        |\n|        O\n|\n|\n|\n|")
+    elif error == 3:
+        print("----------\n|        |\n|        O\n|        |\n|\n|\n|")
+    elif error == 4:
+        print("----------\n|        |\n|        O\n|       /|\n|\n|\n|")
+    elif error == 5:
+        print("----------\n|        |\n|        O\n|       /|\\\n|\n|\n|")
+    elif error == 6:
+        print("----------\n|        |\n|        O\n|       /|\\\n|       /\n|\n|")
+    elif error == 7:
+        print("----------\n|        |\n|        O\n|       /|\\\n|       / \\\n|\n|")
 def main():
     error = 0
     count = 0
     guesses = []
     indices = []
 
-    while error < 8:
+    while error < 7:
         if count == 0:
             word_guess = generate()
             list = ['_' for _ in word_guess]
             print(' '.join(list))
 
+        draw(error)
         guess = input("Make a guess: ").lower()
 
         if len(guess) == 1: # checks if the guess is a single char
@@ -45,7 +63,7 @@ def main():
                 error+=1
 
             if len(guesses) == len(word_guess): # if player wins
-                print(f"You guessed the word: {''.join(guesses)}")
+                print(f"You guessed the word: {word_guess}")
                 try_again = input("Try again? y/n: ").lower()
                 if try_again == 'y':
                     error = 0
@@ -57,12 +75,13 @@ def main():
         else:
             print("Letters only") 
 
-        if error == 7:
+        if error == 6:
             print("Last try!")
 
         count+=1 
     
-    if error == 8:
+    if error == 7:
+        draw(error)
         print(f"You ran out of tries, the word is {word_guess.upper()}")
 
 
